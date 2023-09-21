92d335528ac582e717ea30c796c15d9c3c7c7e7f
ygarcia9 <yolanda.garcia@dxc.com>
2023-09-21 10:26:27 +0200

    kogito_tests: Initial commit

--- src/test/java/org/dedalus/travels/quarkus/ScriptsProcessTest.java
+++ src/test/java/org/dedalus/travels/quarkus/ScriptsProcessTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.acme.travels.quarkus;
+package org.dedalus.travels.quarkus;
 
 import java.util.HashMap;
 import java.util.Map;
