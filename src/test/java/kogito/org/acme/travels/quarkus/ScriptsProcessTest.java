0c335f948402747788e5a767f5420ffe0a5ff0b1
ygarcia9 <yolanda.garcia@dxc.com>
2023-11-24 12:40:56 +0100

    Try to read the two poms separately

--- src/test/java/kogito/org/acme/travels/quarkus/ScriptsProcessTest.java
+++ src/test/java/kogito/org/acme/travels/quarkus/ScriptsProcessTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.acme.travels.quarkus;
+package kogito.org.acme.travels.quarkus;
 
 import java.util.HashMap;
 import java.util.Map;
